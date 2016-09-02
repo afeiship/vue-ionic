5f64a9fea1604ab6808b89e3f87b08baf8cf036b
feizheng <1290657123@qq.com>
2016-09-02 23:02:27 +0800

    update rb file

--- build/async_styles.rb
+++ build/async_styles.rb
@@ -1,11 +1,29 @@
-#!/usr/bin/env ruby -wKU
-require "git";
-
-require "./modules/base_paths";
-require "./modules/env_reset";
+#!/usr/bin/env ruby -wU
+require 'git'
+require 'fileutils'
+require './modules/base_paths'
+require './modules/env_reset'
 include BasePath;
 include EnvRest;
 
-$rootPath= BasePath.get_root_path(Dir.pwd);
-EnvRest.clean_tmp($rootPath);
-Git.clone('git@github.com:afeiship/ionic-sass-helper.git',$rootPath+'/.tmp/ionic-sass-helper');
+ROOT_PATH = BasePath.get_root_path(Dir.pwd);
+IONIC_GIT_REPOSTIRY='git@github.com:afeiship/ionic-sass-helper.git';
+EnvRest.clean(ROOT_PATH);
+
+tmp = File.join(ROOT_PATH, '.tmp');
+tmp_src = File.join(ROOT_PATH, '.tmp','src');
+project_src =File.join(ROOT_PATH, 'src', 'styles');
+
+#clone:
+Git.clone IONIC_GIT_REPOSTIRY, File.join(ROOT_PATH ,'.tmp'),verbose:true;
+puts "clone complete, to next!";
+
+
+#list scss:
+FileUtils.cd(tmp) do
+  Dir.glob('**/{.*,*.*}').reject{ |f| f.include? '.scss' }.each do |item|
+    FileUtils.rm_rf(item);
+  end
+
+  FileUtils.cp_r tmp_src, project_src, verbose: true;
+end
--- build/modules/env_reset.rb
+++ build/modules/env_reset.rb
@@ -1,7 +1,16 @@
 #!/usr/bin/env ruby -wKU
 require "fileutils";
 module EnvRest
-  def clean_tmp(rootPath)
-    FileUtils.rm_f(rootPath+'./tmp');
+
+  def clean(rootPath)
+    # clean .tmp,src/styles
+    tmp_dir = File.join(rootPath,'.tmp');
+    style_dir =  File.join(rootPath,'src','styles');
+    FileUtils.rm_rf tmp_dir;
+    FileUtils.rm_rf style_dir;
+    # midkr
+    FileUtils.mkdir_p tmp_dir;
+    FileUtils.mkdir_p style_dir;
   end
+
 end
