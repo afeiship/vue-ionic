c09527b187b8b26e72c70b285a143f85d3817e30
feizheng <1290657123@qq.com>
2016-09-03 11:10:03 +0800

    install rugged

--- build/async_styles.rb
+++ build/async_styles.rb
@@ -11,20 +11,22 @@ ROOT_PATH = BasePath.get_root_path(Dir.pwd);
 GIT_REPOSTIRY='git@github.com:afeiship/ionic-sass-helper.git';
 EnvRest.clean(ROOT_PATH);
 
-tmp = File.join(ROOT_PATH, '.tmp');
-tmp_src = File.join(ROOT_PATH, '.tmp','src');
-project_src =File.join(ROOT_PATH, 'src', 'styles');
-
-#clone:
-g = Git.clone GIT_REPOSTIRY, tmp;
-g.log.each {|l| puts l.sha };
-
-
-#list scss:
-FileUtils.cd(tmp) do
-  Dir.glob('**/{.*,*.*}').reject{ |f| f.include? '.scss' }.each do |item|
-    FileUtils.rm_rf(item);
-  end
-
-  FileUtils.cp_r tmp_src, project_src, verbose: true;
-end
+puts Rugged::Repository.discover();
+# 
+# tmp = File.join(ROOT_PATH, '.tmp');
+# tmp_src = File.join(ROOT_PATH, '.tmp','src');
+# project_src =File.join(ROOT_PATH, 'src', 'styles');
+#
+# #clone:
+# g = Git.clone GIT_REPOSTIRY, tmp;
+# g.log.each {|l| puts l.sha };
+#
+#
+# #list scss:
+# FileUtils.cd(tmp) do
+#   Dir.glob('**/{.*,*.*}').reject{ |f| f.include? '.scss' }.each do |item|
+#     FileUtils.rm_rf(item);
+#   end
+#
+#   FileUtils.cp_r tmp_src, project_src, verbose: true;
+# end
