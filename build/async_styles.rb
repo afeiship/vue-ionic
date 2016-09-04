d138bce5358a4421f144bed2d1fee93c25d99c6f
feizheng <1290657123@qq.com>
2016-09-04 15:15:27 +0800

    update asynic styles build script

--- build/async_styles.rb
+++ build/async_styles.rb
@@ -2,33 +2,42 @@
 require 'git'
 require 'rugged'
 require 'fileutils'
-require './modules/base_paths'
+require 'ruby-progressbar';
 require './modules/env_reset'
-include BasePath;
-include EnvRest;
+include EnvRest
 
-ROOT_PATH = BasePath.get_root_path(Dir.pwd);
-GIT_REPOSTIRY='git@github.com:afeiship/ionic-sass-helper.git';
-EnvRest.clean(ROOT_PATH);
+ROOT_PATH = File.dirname Rugged::Repository.discover.path
+GIT_REPOSTIRY = 'https://github.com/driftyco/ionic.git';
+EnvRest.clean(ROOT_PATH)
 
-g= Rugged::Repository.discover();
-puts g.path;
-# puts Dir.basename g.path;
-#
-# tmp = File.join(ROOT_PATH, '.tmp');
-# tmp_src = File.join(ROOT_PATH, '.tmp','src');
-# project_src =File.join(ROOT_PATH, 'src', 'styles');
-#
-# #clone:
-# g = Git.clone GIT_REPOSTIRY, tmp;
-# g.log.each {|l| puts l.sha };
-#
-#
-# #list scss:
-# FileUtils.cd(tmp) do
-#   Dir.glob('**/{.*,*.*}').reject{ |f| f.include? '.scss' }.each do |item|
-#     FileUtils.rm_rf(item);
-#   end
-#
-#   FileUtils.cp_r tmp_src, project_src, verbose: true;
-# end
+progressbar = ProgressBar.create( :format => "%a %b %c/%C\u{15E7}%i %p%% %t",
+                    :progress_mark  => '=',
+                    :throttle_rate => 0.1,
+                    :remainder_mark => "\u{FF65}",
+                    :starting_at    => 0)
+tmp = File.join(ROOT_PATH, '.tmp');
+tmp_src = File.join(ROOT_PATH, '.tmp','src');
+project_src =File.join(ROOT_PATH, 'src', 'styles');
+
+# puts Rugged::Repository.attributes_name;
+Rugged::Repository.clone_at(GIT_REPOSTIRY, tmp, {
+  checkout_branch:'master',
+  transfer_progress:lambda { |total_objects, indexed_objects, received_objects, local_objects, total_deltas, indexed_deltas, received_bytes|
+    if received_objects == 0
+      progressbar.total = total_objects;
+    end
+
+    if (received_objects < total_objects) && (progressbar.progress < progressbar.total)
+      progressbar.increment;
+    end
+  }
+});
+
+#list scss:
+FileUtils.cd(tmp) do
+  Dir.glob('**/{.*,*.*}').reject{ |f| f.include? '.scss' }.each do |item|
+    FileUtils.rm_rf(item);
+  end
+
+  FileUtils.cp_r tmp_src, project_src;
+end
