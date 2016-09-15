983511e0077418148b89f8112f35c61ec00170d4
feizheng <1290657123@qq.com>
2016-09-15 08:53:56 +0800

    add grid docs

---
+++ src/widgets/checkbox/src/index.vue
@@ -0,0 +1,25 @@
+<template lang="html">
+  <ion-checkbox checked="true">
+      <div class="checkbox-icon checkbox-checked">
+        <div class="checkbox-inner"></div>
+      </div>
+      <button category="item-cover" class="item-cover disable-hover item-cover-default" role="checkbox" type="button" ng-reflect-category="item-cover" ng-reflect-id="chk-43-0" id="chk-43-0" aria-checked="true" aria-labelledby="lbl-43" aria-disabled="false"><span class="button-inner">
+      </span><ion-button-effect></ion-button-effect></button>
+  </ion-checkbox>
+</template>
+
+<script>
+export default {
+  data() {
+      return {};
+    },
+    computed: {},
+    ready() {},
+    attached() {},
+    methods: {},
+    components: {}
+};
+</script>
+
+<style lang="css">
+</style>
