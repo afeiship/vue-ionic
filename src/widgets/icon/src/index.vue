9952d89ffd79bbc5e0833ae7e004503d99bde8d3
feizheng <1290657123@qq.com>
2016-09-15 23:33:53 +0800

    add small props

--- src/widgets/icon/src/index.vue
+++ src/widgets/icon/src/index.vue
@@ -1,10 +1,19 @@
 <template lang="html">
-  <ion-icon class="ion ion-{{name}}"></ion-icon>
+  <ion-icon class="ion ion-{{name}}" :small="small"></ion-icon>
 </template>
 
 <script>
 export default {
-  name:'ViIcon',
-  props:['name']
+  name: 'ViIcon',
+  props: {
+    name: {
+      type: String,
+      default: null
+    },
+    small: {
+      type: Boolean,
+      default: true
+    }
+  }
 };
 </script>
